1a2a41724dde71614e160e772d5771f0d6bb5114
Beatriz Ramerindo <beatriz.ramerindo@creditas.com>
2022-06-11 14:30:15 -0300

    configuracao do banco

---
+++ api_com_mongo/anotacoes.md
@@ -0,0 +1,11 @@
+
+-> /api_com_mogo
+
+touch server.js
+npm init -y
+touch .env
+npm i express
+npm i cors
+npm i dotenv
+npm i mongoose
+npm i -D nodemon
--- api_com_mongo/src/app.js
+++ api_com_mongo/src/app.js
@@ -1,1 +1,15 @@
 
+const express = require('express')
+require('dotenv').config()
+const cors = require('cors')
+const db = require('./database/mongooseConnect')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+db.connect()
+
+
+module.exports = app
+
