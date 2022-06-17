4667613e5a343ccfa914a38db272c476e2054c5d
BrunaCelestino <brunaccelestino@gmail.com>
2022-06-17 16:44:47 -0300

    Rotas patch put e gets adicionadas

--- para_o_lar/src/controllers/artistsController.js
+++ para_o_lar/src/controllers/artistsController.js
@@ -26,45 +26,9 @@ const createArtist = async (request, response)=> {
 }
 
 const findAllArtists = async (request, response) => {
-    const {birthName, artisticName} = request.query
-    let query =  {}
-
-    
-
 try {
-    if(birthName) {
-        query.birthName = new RegExp(birthName, "i")
-        const allArtists = await ArtistModel.find(query)
 
-        if (allArtists.length === 0) {
-            throw {
-                statusCode: 404,
-                message: "Não encontramos resultados com essa busca.",
-                details: "Em nosso banco de dados, não existem informações compatíveis com essa busca.",
-                query: request.query
-            }
-
-     }
-    }
-
-
-    if(artisticName) {
-        query.artisticName = new RegExp(artisticName, "i") 
-        const allArtists = await ArtistModel.find(query)
-
-        if (allArtists.length === 0) {
-            throw {
-                statusCode: 404,
-                message: "Não encontramos resultados com essa busca.",
-                details: "Em nosso banco de dados, não existem informações compatíveis com essa busca.",
-                query: request.query
-            }
-
-     }
-    }
-
-    
-    const allArtists = await ArtistModel.find(query)
+    const allArtists = await ArtistModel.find()
 
     response.status(200).json(allArtists)
 } catch (error) {
@@ -77,6 +41,82 @@ try {
         }
     }
 }
+
+const findById = async (request, response) => {
+
+    try {        
+        const id = request.params.id;
+        await ArtistModel
+        .findById(id)       
+        .exec((err, artists) => {
+          if (err) {
+            response.status(400).json({ message: `Id informado está fora do padrão.` ,
+        details: err.message });
+          } else if (artists == null) {
+            response.status(404).json({ message: 'Id do artista não encontrado na base de dados'});
+          } else {
+            response.status(200).json(artists);
+          }
+        });
+    } catch (error) {
+        
+            response.status(500).json({ message: error.message })
+        
+    }
+    }
+
+const findByName = async (request, response) => {
+    let {artisticName, birthName } = request.query 
+    let query = {}    
+       try {   
+           if(birthName) {
+               query.birthName = new RegExp(birthName, "i")
+               const allArtists = await ArtistModel.find(query)
+       
+               if (allArtists.length === 0) {
+                   throw {
+                       statusCode: 404,
+                       message: "Não encontramos resultados com essa busca.",
+                       details: "Em nosso banco de dados, não existem informações compatíveis com essa busca.",
+                       query: request.query
+                   }
+       
+            }
+           }
+       
+       
+           if(artisticName) {
+               query.artisticName = new RegExp(artisticName, "i") 
+               const allArtists = await ArtistModel.find(query)
+       
+               if (allArtists.length === 0) {
+                   throw {
+                       statusCode: 404,
+                       message: "Não encontramos resultados com essa busca.",
+                       details: "Em nosso banco de dados, não existem informações compatíveis com essa busca.",
+                       query: request.query
+                   }
+       
+            }
+           }
+       
+           
+           const allArtists = await ArtistModel.find(query)
+       
+           response.status(200).json({"query": request.query,
+           "Artistas encontradas": allArtists.length,
+       "Lista de artistas": allArtists})
+      
+   }catch (error) {
+       console.error(error)
+       console.log("Busca recebida: ", request.query)
+       if (error.statusCode) {
+           response.status(error.statusCode).json(error)
+       } else {
+           response.status(500).json({ message: error.message })
+       }
+   }
+   }
 
 const findByOccupation = async (request, response) => {
     const {occupation} = request.query
@@ -109,11 +149,97 @@ const findByOccupation = async (request, response) => {
     }
     }
 
+    
+    
+    
+
+    const updateArtistItems = async (request, response) => {
+       
+        try {
+            const findArtist = await ArtistModel.findById(request.params.id)
+
+            if (findArtist == undefined) {
+                throw {
+                    statusCode: 404,
+                    message: "Não encontramos resultados com essa busca.",
+                    details: "Em nosso banco de dados, não existem informações compatíveis com essa busca.",
+                    query: request.query
+                }}
+
+            const updateArtist = await ArtistModel.findOneAndUpdate(findArtist, request.body)
+
+           
+                response.status(200).json({
+                    "artista atualizado": updateArtist
+                })
+            
+    
+        } catch (error) {
+        if (error.statusCode) {
+            response.status(error.statusCode).json(error)
+        } else {
+            response.status(500).json({ message: error.message })
+        }
+    }
+        }
+
+        const updateAllArtistItems = async (request, response) => {
+            const{
+                birthName, artisticName, age, birthday, from, occupation, bio, famousWorks, awards, alive, image
+            } = request.body
+            try {
+                const findArtist = await ArtistModel.findById(request.params.id)
+    
+                if (findArtist == undefined) {
+                    throw {
+                        statusCode: 404,
+                        message: "Não encontramos resultados com essa busca.",
+                        details: "Em nosso banco de dados, não existem informações compatíveis com essa busca.",
+                        query: request.query
+                    }}
+    
+                    const updateAll = {birthName, artisticName, age, birthday, from, occupation, bio, famousWorks, awards, alive, image}
+
+                    const keys = Object.keys(updateAll)
+
+                    keys.forEach(key => {
+                        if (!updateAll[key]) {
+                            throw {
+                                statusCode: 406,
+                                message: `Não foi possível atualizar artista: ${request.params.id}. Todos os itens devem ser preenchidos.`,
+                                details: `Para atualizar artista, é preciso preencher todos os dados.`
+                            };
+                        };
+
+                    });
+
+                const updateArtist = await ArtistModel.findOneAndReplace(request.params.id, request.body)
+    
+               
+                    response.status(200).json({
+                        "artista atualizado": updateArtist
+                    })
+                
+        
+            } catch (error) {
+            if (error.statusCode) {
+                response.status(error.statusCode).json(error)
+            } else {
+                response.status(500).json({ message: error.message })
+            }
+        }
+            }
+    
+    
 
 
 module.exports = {
     createArtist,
     findAllArtists,
-    findByOccupation
+    findById,
+    findByName,
+    findByOccupation,
+    updateArtistItems,
+    updateAllArtistItems
 
 }
--- para_o_lar/src/routes/artistsRoutes.js
+++ para_o_lar/src/routes/artistsRoutes.js
@@ -8,6 +8,14 @@ router.post("/new_artist", controller.createArtist);
 
 router.get("/all", controller.findAllArtists);
 
+router.get("/by_id/:id", controller.findById);
+
+router.get("/by_name", controller.findByName);
+
 router.get("/occupation", controller.findByOccupation);
+
+router.patch("/update_item/:id", controller.updateArtistItems);
+
+router.put("/update_all/:id", controller.updateAllArtistItems);
 
 module.exports = router
