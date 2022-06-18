53840a3abcb837050ea5dacb07f439fe753e125a
nathaalua <nmarques2107@gmail.com>
2022-06-18 08:15:30 -0300

    exercicio

---
+++ api_com_mongo/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+require("dotenv").config();
+
+const cors = require("cors");
+
+const db = require("./database/mongooseConnect")
+const musicRoutes = require("./routes/musicRoutes.js");
+
+const app = express()
+
+
+
+app.use(express.json())
+app.use(cors())
+
+db.connect()
+
+app.use("/music", musicRoutes)
+
+module.exports = app
+
+
+
+const express = require('express')
+require('dotenv').config()
